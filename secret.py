2e1fb05876ebaf310a6a13499d4626dc2e9e7b7b
Brian <godonangaming@gmail.com>
2021-11-16 13:58:58 -0600

    Better secret.py that allows public safe github pushes

---
+++ secret.py
@@ -0,0 +1,7 @@
+import os
+
+app_id = str(os.getenv('R_APPID'))
+username = str(os.getenv('R_USERNAME'))
+password = str(os.getenv('R_PASSWORD'))
+client_id = str(os.getenv('R_CLIENT_ID'))
+secret = str(os.getenv('R_SECRET'))
